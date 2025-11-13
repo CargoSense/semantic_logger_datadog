834d28469bfcb60ef2fd26eb022a51038d7bbeb7
Jason Garber <jason.garber@cargosense.com>
2025-11-13 15:44:25 +0000

    Wrap configuration in initializer
    See-also: https://api.rubyonrails.org/classes/Rails/Railtie.html

--- lib/semantic_logger_datadog/railtie.rb
+++ lib/semantic_logger_datadog/railtie.rb
@@ -8,23 +8,25 @@ end
 
 module SemanticLoggerDatadog
   class Railtie < Rails::Railtie
-    # Set log output format.
-    #
-    # @see https://logger.rocketjob.io/rails.html#output-format
-    config.rails_semantic_logger.format = SemanticLoggerDatadog::Formatters::Json.new
+    initializer "semantic_logger_datadog.configure_rails_initialization" do |app|
+      # Set log output format.
+      #
+      # @see https://logger.rocketjob.io/rails.html#output-format
+      app.config.rails_semantic_logger.format = SemanticLoggerDatadog::Formatters::Json.new
 
-    # Set backtrace level to avoid memory leaks due to high object allocation.
-    #
-    # @see https://logger.rocketjob.io/rails.html#include-the-file-name-and-line-number-in-the-source-code-where-the-message-originated
-    config.semantic_logger.backtrace_level = :error if Rails.env.production?
+      # Set backtrace level to avoid memory leaks due to high object allocation.
+      #
+      # @see https://logger.rocketjob.io/rails.html#include-the-file-name-and-line-number-in-the-source-code-where-the-message-originated
+      app.config.semantic_logger.backtrace_level = :error if Rails.env.production?
 
-    # Log to standard output when +RAILS_LOG_TO_STDOUT+ is configured.
-    #
-    # @see https://logger.rocketjob.io/rails.html#log-to-standard-out
-    if ENV["RAILS_LOG_TO_STDOUT"].present?
-      $stdout.sync = true
-      config.rails_semantic_logger.add_file_appender = false
-      config.semantic_logger.add_appender(formatter: config.rails_semantic_logger.format, io: $stdout)
+      # Log to standard output when +RAILS_LOG_TO_STDOUT+ is configured.
+      #
+      # @see https://logger.rocketjob.io/rails.html#log-to-standard-out
+      if ENV["RAILS_LOG_TO_STDOUT"].present?
+        $stdout.sync = true
+        app.config.rails_semantic_logger.add_file_appender = false
+        app.config.semantic_logger.add_appender(formatter: config.rails_semantic_logger.format, io: $stdout)
+      end
     end
   end
 end
