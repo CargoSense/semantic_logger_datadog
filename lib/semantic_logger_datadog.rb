8b6e99beb3183a0b51eda001af2faebab5822441
Jason Garber <jason.garber@cargosense.com>
2025-11-13 14:05:42 +0000

    Require semantic_logger
    Rather than only requiring the raw formatter, require the whole of
    Semantic Logger. This should make using this gem a bit easier in cases
    where an app isn't also using rails_semantic_logger.

--- lib/semantic_logger_datadog.rb
+++ lib/semantic_logger_datadog.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "semantic_logger/formatters/raw"
+require "semantic_logger"
 
 require_relative "semantic_logger_datadog/formatters/raw"
 require_relative "semantic_logger_datadog/formatters/json"
