6010e704d550aaa3ddfa2c205fb98bb4a216121e
Jason Garber <jason.garber@cargosense.com>
2026-09-23 18:20:07 +0000

    Use Railtie's `config` directly
    Might address the issue here:
    
    > [rails_semantic_logger] `config.rails_semantic_logger.appenders` was
    > set too late to take effect, so it has no effect. Set it in
    > `config/application.rb` or `config/environments/production.rb`;
    > `config/initializers/*` is loaded too late.

--- lib/semantic_logger_datadog/railtie.rb
+++ lib/semantic_logger_datadog/railtie.rb
@@ -10,6 +10,16 @@ require_relative "middleware"
 
 module SemanticLoggerDatadog
   class Railtie < Rails::Railtie
+    # Log to standard output when +RAILS_LOG_TO_STDOUT+ is configured.
+    #
+    # @see https://logger.reidmorrison.com/rails.html#output-formats
+    # @see https://logger.reidmorrison.com/rails.html#production-on-a-container-platform-docker-kubernetes-heroku
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      config.rails_semantic_logger.appenders do |appenders|
+        appenders.add_server(formatter: SemanticLoggerDatadog::Formatters::Json.new)
+      end
+    end
+
     initializer "semantic_logger_datadog.configure_rails_initialization" do |app|
       # Set application name.
       app.config.semantic_logger.application = Rails.application.name
@@ -18,16 +28,6 @@ module SemanticLoggerDatadog
       #
       # @see https://logger.reidmorrison.com/rails.html#source-file-name-and-line-number
       app.config.semantic_logger.backtrace_level = :error if Rails.env.production?
-
-      # Log to standard output when +RAILS_LOG_TO_STDOUT+ is configured.
-      #
-      # @see https://logger.reidmorrison.com/rails.html#output-formats
-      # @see https://logger.reidmorrison.com/rails.html#production-on-a-container-platform-docker-kubernetes-heroku
-      if ENV["RAILS_LOG_TO_STDOUT"].present?
-        app.config.rails_semantic_logger.appenders do |appenders|
-          appenders.add_server(formatter: SemanticLoggerDatadog::Formatters::Json.new)
-        end
-      end
 
       app.middleware.insert_before RailsSemanticLogger::Rack::Logger, SemanticLoggerDatadog::Middleware
     end
